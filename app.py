6c854699bafd58253eccc36b42b86f48d64f3136
gdgsummercamp <hskang9@gmail.com>
2017-10-28 00:56:05 +0900

    accept form data

--- app.py
+++ app.py
@@ -213,7 +213,7 @@ def mine():
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
-    values = request.get_json()
+    values = request.get_json() or request.form
 
     # Check that the required fields are in the POST'ed data
     required = ['sender', 'recipient', 'amount']
